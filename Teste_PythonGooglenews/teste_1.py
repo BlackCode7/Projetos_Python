07c5fddbb06ba2732962b70cb1a5f5c5890810a7
Anderson Martins <anderson77martins@outlook.com>
2021-05-09 17:59:06 -0300

    Adicinando lib de raspagem de noticias GooGleNews

---
+++ Teste_PythonGooglenews/teste_1.py
@@ -0,0 +1,11 @@
+from GoogleNews import GoogleNews
+googlenews = GoogleNews()
+googlenews = GoogleNews(period='7d')
+googlenews.search('USA')
+result = googlenews.result()
+for x in result:
+    print('-' * 50)
+    print('Title -- ', x['title'])
+    print('Date / Time -- ', x['date'])
+    print('Description -- ', x['desc'])
+    print('Link -- ', x['link'])
